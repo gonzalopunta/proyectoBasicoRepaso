503e793dfb9079d9b2c45dec48d0890d683a7d96
gonzalopunta <gpuntaperez@safareyes.es>
2020-10-30 18:46:13 +0100

    Entregar

--- productos/src/main/java/com/proyecto/productos/controllers/UsuarioController.java
+++ productos/src/main/java/com/proyecto/productos/controllers/UsuarioController.java
@@ -1,4 +1,23 @@
 package com.proyecto.productos.controllers;
 
+import com.proyecto.productos.models.Usuario;
+import com.proyecto.productos.services.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @GetMapping("/")
+    public String iniciarSesion(Usuario usuario, Model u){
+        u.addAttribute("usuario", usuario);
+        return "index";
+    }
 }
